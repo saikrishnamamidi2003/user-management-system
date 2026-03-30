b376ca08604dd7d29bcb8dfd63d55b88a2d9b9f9
sai krishna <saikrishnamamidi2003@gmail.com>
2026-03-30 18:43:10 +0530

    feat: added REST controller for user CRUD APIs

--- user-management/src/main/java/com/saikrishna/user_management/service/UserService.java
+++ user-management/src/main/java/com/saikrishna/user_management/service/UserService.java
@@ -1,5 +1,36 @@
 package com.saikrishna.user_management.service;
 
-public class UserService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.saikrishna.user_management.entity.User;
+import com.saikrishna.user_management.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	//create user
+	public User saveUser(User user) {
+		return userRepository.save(user);
+	}
+	
+	//get all users
+	public List<User> getAllUsers(){
+		return userRepository.findAll()	;
+		}
+	//update user
+	public User updateUser(Long id, User user) {
+		user.setId(id);
+		return userRepository.save(user);
+	}
+	
+	//Delete user
+	public void deleteUser(Long id) {
+		userRepository.deleteById(id);
+	}
 }
