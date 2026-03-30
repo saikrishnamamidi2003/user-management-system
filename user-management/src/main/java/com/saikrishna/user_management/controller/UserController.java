31b67999c018ad9b48b21d238a0f8ac2fe35e9b3
sai krishna <saikrishnamamidi2003@gmail.com>
2026-03-30 21:18:12 +0530

    react - frontend setup

--- user-management/src/main/java/com/saikrishna/user_management/controller/UserController.java
+++ user-management/src/main/java/com/saikrishna/user_management/controller/UserController.java
@@ -1,5 +1,39 @@
 package com.saikrishna.user_management.controller;
 
-public class UserController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.saikrishna.user_management.entity.User;
+import com.saikrishna.user_management.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping
+	public User createUser(@RequestBody User user) {
+		return userService.saveUser(user);
+	}
+	
+	//Read
+	@GetMapping
+	public List<User> getAllUsers(){
+		return userService.getAllUsers();
+	}
+	
+	//Update
+	@PutMapping("/{id}")
+	public User updateUser(@PathVariable Long id, @RequestBody User user) {
+		return userService.updateUser(id, user);
+	}
+	
+	//Delete
+	@DeleteMapping("{id}")
+	public void deleteUser(@PathVariable Long id) {
+		userService.deleteUser(id);
+	}
 }
