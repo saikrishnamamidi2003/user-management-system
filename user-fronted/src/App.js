6c3ccfc56b310ba251a24a78e3bd7b90bb065559
sai krishna <saikrishnamamidi2003@gmail.com>
2026-03-30 22:23:07 +0530

    update app.js

--- user-fronted/src/App.js
+++ user-fronted/src/App.js
@@ -1,7 +1,12 @@
+import UserForm from "./components/UserForm";
+import UserList from "./components/UserList";
+
 function App() {
   return (
     <div>
       <h1>User Management System</h1>
+      <UserForm />
+      <UserList />
     </div>
   );
 }
